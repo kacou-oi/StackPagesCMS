e0ac279c2ab9d4ff91fa6c38a080a6ef23b0086d
Ange Kacou Oi <ange.kacou.oi@gmail.com>
2025-08-11 11:11:05 +0000

    enable autoplay

--- js/player.js
+++ js/player.js
@@ -57,7 +57,7 @@ document.addEventListener('DOMContentLoaded', async () => {
                 class="w-full h-full"
                 src="https://www.youtube.com/embed/${video.id}" 
                 frameborder="0" 
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                allow="accelerometer; autoplay=0; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
                 allowfullscreen>
             </iframe>
         `;
